3983bc81ad362639e1ca6966a861237f3b2fb368
Marcos Antônio Vieira de Sá <marcosantonio3232@hotmail.com>
2018-07-21 00:03:14 -0300

    #1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,24 @@ import { EntrarComponent } from './pages/acount/entrar/entrar.component';
 import { RecuperarSenhaComponent } from './pages/acount/recuperar-senha/recuperar-senha.component';
 import { AcountComponent } from './pages/acount/acount.component';
 import { HomeComponent } from './pages/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const ROUTES: Routes = [
     { path: '', redirectTo: 'acount', pathMatch: 'full' },
-    { path: 'acount', component: AcountComponent, children: [
-        { path: '', redirectTo: 'entrar', pathMatch: 'full' },
-        { path: 'entrar', component: EntrarComponent },
-        { path: 'recuperar-senha', component: RecuperarSenhaComponent }
-    ] },
-    { path: 'home', component: HomeComponent}
+    {
+        path: 'acount',
+        component: AcountComponent,
+        children: [
+            { path: '', redirectTo: 'entrar', pathMatch: 'full' },
+            { path: 'entrar', component: EntrarComponent },
+            { path: 'recuperar-senha', component: RecuperarSenhaComponent }
+        ]
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+        children: [
+            { path: '', component: DashboardComponent },
+        ]
+    }
 ];
