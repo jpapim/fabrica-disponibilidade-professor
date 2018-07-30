5e6cf25f5925ced4f85982bc4ce45035ec3b5206
dev-yves <yveslopo@gmail.com>
2018-07-30 00:15:12 -0300

    #1 - Ajustes no ConfigurationForm dos dados profissionais

--- src/app/components/forms/form-dados-profissionais/form-dados-profissionais.component.ts
+++ src/app/components/forms/form-dados-profissionais/form-dados-profissionais.component.ts
@@ -15,17 +15,25 @@ export class FormDadosProfissionaisComponent implements OnInit {
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-     $('.datepicker').datepicker();
+    $('.datepicker').datepicker();
     this.configurationForm();
   }
 
   configurationForm() {
     this.dadosProfissionaisForm = this.formBuilder.group({
-      inputGraduacao: [ null, [Validators.required, Validators.minLength(3)]],
-      inputDataConclusao: [ null, [Validators.required]],
-      inputMaiorTitulacao: [ null, [Validators.required]],
-      inputNomeTitulacaoAndamento: [ null, [Validators.required, Validators.minLength(3)]],
-      inputEstimativaTermino: [ null, [Validators.required]]
+      inputGraduacao: [null, [Validators.required, Validators.minLength(3)]],
+      inputDataConclusao: [null, [Validators.required]],
+      inputMaiorTitulacao: [null, [Validators.required]],
+      inputTitulacaoAndamento: [null, [Validators.required]],
+      inputNomeTitulacaoAndamento: [null, [Validators.required, Validators.minLength(3)]],
+      inputEstimativaTermino: [null, [Validators.required]],
+      inputPrincipalAtuacaoProfissional: [null, [Validators.required, Validators.minLength(20)]],
+      inputTempoXpProfissional: [null, [Validators.required, Validators.minLength(2)]],
+      inputTempoXpMagisterioSuperior: [null, [Validators.required, Validators.minLength(2)]],
+      inputXpDocenciaEducacaoBasica: [null, [Validators.required, Validators.minLength(2)]],
+      inputDataAttLattes: [null, [Validators.required]],
+      inputUrlLattes: [null, [Validators.required]],
+      inputTxtAreaPublicacoes: [null, [Validators.required]]
     });
   }
 
